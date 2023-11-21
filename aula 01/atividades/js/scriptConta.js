b6fd7224d78349a5ff1468e42f96b82bb78ed5c8
MatheusHLima15 <matheushsousa15@gmail.com>
2023-11-21 11:43:38 -0300

    JavaScript

--- aula 01/atividades/js/scriptConta.js
+++ aula 01/atividades/js/scriptConta.js
@@ -1,11 +1,6 @@
-function calcularConta(numClientes, valorTotal) {
-    if (isNaN(numClientes) || isNaN(valorTotal) || numClientes <= 0 || valorTotal <= 0) {
-        console.log('Por favor, insira valores válidos.');
-        return;
-    }
-    var valorPorCliente = valorTotal / numClientes;
+var clientes = Number(prompt("Quantos clientes estão na mesa?"))
+var valorTotal = Number(prompt("Qual é o valor total da conta?"))
 
-    console.log('Cada cliente deve pagar: R$ ' + valorPorCliente.toFixed(2));
-}
+var valorDividido = valorTotal / clientes;
 
-calcularConta(4, 100); 
+alert(`Cada um vai pagar  + ${valorDividido}`);
