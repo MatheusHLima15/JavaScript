16fe8609caa4f5cde4977ee3f28471ef322a3afc
MatheusHLima15 <matheushsousa15@gmail.com>
2023-11-21 10:20:34 -0300

    Introdução o JS

---
+++ aula 01/Material de aula/js/script01.js
@@ -0,0 +1,57 @@
+// caixas de texto
+
+// alert("Olá Mundo");
+// confirm("Bom dia, o dia está bom?");
+// prompt("Qual é a sua idade?");
+
+// console.log("Olha que legal!");
+
+// Variaveis
+// Let "temporaria, é sobreescrevivel,escopo global"
+function letTeste(){
+    let numero = 0;
+    numero = 1;
+    console.log(numero)
+}
+// var variavel, recebe valores e é sobreescrevivel, escopo global
+var nome= "Matheus"
+// const constante, não sobreescrevivel,escopo global
+const pi= 3.14
+
+const name = prompt("qual é o seu nome?");
+// concatenação
+alert("seja bem-vindo, " + name);
+alert(`${name}, aproveite a estadia!`);
+// `` comando acima usa crase
+// ${} desconsidera elementos como texto ou string
+
+// tipos de valores
+// string
+var texto = "olá mundo";
+// number
+var numeroBacana = 100;
+// boolean
+var ligado = true;
+// (objeto)
+var carro = [atributo ="algo"];
+// array
+// var compras = {"Açucar", "arroz"}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ aula 01/Material de aula/js/script02.js
@@ -0,0 +1,9 @@
+alert("calculadora de Soma");
+var numero1 = Number(prompt("Digite o seu primeiro numero"));
+var mumero2 = Number(prompt("Digite o seu segundo numero"));
+
+// parseFloat converte de string para numero
+// var soma = parseFloat(numero1) + parseFloat(numero2);
+var soma = (numero1) + (numero2);
+
+alert(`o resultado da soma é de:${soma}`);
