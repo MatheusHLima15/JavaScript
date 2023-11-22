7df40cb6549f7f63d86d4201e6b5052e74411cc1
MatheusHLima15 <matheushsousa15@gmail.com>
2023-11-22 08:32:49 -0300

    javaScript

--- aula 01/atividades/js/scriptNum.js
+++ aula 01/atividades/js/scriptNum.js
@@ -1,14 +1,21 @@
-function mostrarSucessorAntecessor(numeroDigitado) {
-    if (isNaN(numeroDigitado)) {
-        console.log('Por favor, insira um número válido.');
-        return;
-    }
+// function mostrarSucessorAntecessor(numeroDigitado) {
+//     if (isNaN(numeroDigitado)) {
+//         console.log('Por favor, insira um número válido.');
+//         return;
+//     }
 
-    var antecessor = numeroDigitado - 1;
-    var sucessor = numeroDigitado + 1;
+//     var antecessor = numeroDigitado - 1;
+//     var sucessor = numeroDigitado + 1;
    
-    console.log('Antecessor: ' + antecessor + '\nNúmero Escolhido: ' + numeroDigitado + '\nSucessor: ' + sucessor);
-}
+//     console.log('Antecessor: ' + antecessor + '\nNúmero Escolhido: ' + numeroDigitado + '\nSucessor: ' + sucessor);
+// }
 
 
-mostrarSucessorAntecessor(5); 
+// mostrarSucessorAntecessor(5); 
+
+var numero = Number(prompt("Qual é o número desejado"))
+
+var sucessor = numero + 1;
+var antecessor = numero - 1;
+
+alert(`O antecessor`)
