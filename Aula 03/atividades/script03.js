6e42f83375bef20b686c7fcf60a40a0ab6964ae3
Matheus Lima <matheushsousa15@gmail.com>
2023-11-23 21:06:24 -0300

    conclusão atividade 03
    conclusão da aula 03

--- Aula 03/atividades/script03.js
+++ Aula 03/atividades/script03.js
@@ -1,24 +1,8 @@
-const um = 1
-const dois = 2
-const tres = 3
-const quatro = 4
-const cinco = 5
-const seis = 6
-const sete = 7
-const oito = 8
-const nove = 9
-const dez = 10
-var tabuada = 1
-var resultado = +1
-
-do{
-    var resultado = um * tabuada
-    console.log(`${um} x ${tabuada} = ${resultado}`);
-tabuada++;
-}while(tabuada <= 10 & resultado >= 0);
-
-do{
-    var resultado = dois * tabuada
-    console.log(`${dois} x ${tabuada} = ${resultado}`);
-tabuada++;
-}while(tabuada <= 10 & resultado >= 0);
+for (let numero = 1; numero <= 10; numero++) {
+    console.log("Tabuada do " + numero + ":");
+    for (let tabuada = 1; tabuada <= 10; j++) {
+      const resultado = numero * tabuada;
+      console.log(numero + " x " + tabuada + " = " + resultado);
+    }
+    console.log("----------------------");
+  }
