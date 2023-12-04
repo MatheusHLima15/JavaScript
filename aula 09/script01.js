40d9358147d7397d49baba901a1468491c331398
MatheusHLima15 <matheushsousa15@gmail.com>
2023-12-04 11:40:03 -0300

    aulas 8 e 9
    up

--- aula 09/script01.js
+++ aula 09/script01.js
@@ -1,15 +1,44 @@
-//INSTALAÇÃO
-//VISITE https://jquery.com/download/
-//CLICAR NA OPÇÃO "DOWNLOAD UNCOMPRESSED j-query 3.7.1"
-//ABERTO O ARQUIVO, APERTE COM O BOTÃO DIREITO E SALVAR PÁGINA COMO.. E SALVE EM UM LOCAL DA SUA PREFERENCIA
+// Instalação
+// VISITE https://jquery.com/download/
+// clicar na opção "Download the uncompressed, development jQuery 3.7.1"
+// Aberto o arquivo, aperte com o botão direito e salvar página como.. e salve em um local de sua preferência!
 
 
+// Seletor JQUERY = $("# ou . ou a prórpia tag")
+var titulo = $("#tituloDaPagina").text("JQUERY AULA")
+console.log(titulo)
 
-//SELETOR JQUERY = $(# ou .elemento)
-var titulo = $("#tituloDaPagina").text("Jquery Aula");
-console.log(titulo);
+// -------------------------------------------
+// Sem JQUERRY
 
-var paragrafo = $("textoDaPagina").on("mouseover",function(){
-    console.log("Este é o texto do site");
+// var paragrafo = document.querySelector("#textoDaPagina")
 
-});
+// paragrafo.addEventListener("mouseover", consoleListar)
+
+// function consoleListar(){
+//     console.log("Este é o texto do site")
+// }
+
+
+            // Com JQUERRY
+var paragrafo = $("#textoDaPagina").on("mouseover", function (){
+    console.log("Este é o texto do site")
+})
+
+// ----------------------------------------
+
+//comportamento
+// JQUERY tem capacidade de avaliar valores não definidos e vazios
+var usuario = "Victor"
+var usuarioIndefinido = undefined
+
+var usuarioResultado = $("#testeIndefinido").text(undefined)
+
+//algoritmo contagem de caracteres
+var blocoDeTexto = $("#blocoDeTexto").on("input", function(){
+    let blocoDeTextoValue = $("#blocoDeTexto").val()
+    let resposta = $("#numCaracteres")
+    
+    var contagem =  blocoDeTextoValue.length
+    resposta.text(contagem)
+})
